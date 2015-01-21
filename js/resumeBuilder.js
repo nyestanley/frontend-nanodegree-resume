10c6847a0b245ef8dbd20a833e3205ec0edbaadc
Niesha S. (nyestanley) <nieshaamos@gmail.com>
2015-01-21 16:11:37 -0500

    1/21/2015 work - Almost done with Lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,15 +5,15 @@ console.log(awesomeThoughts);
 
 var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 
-$("#main").append(funThoughts);
+$("#main").append(funThoughts);*/
 
 var formattedName = HTMLheaderName.replace("%data%", "Niesha Stanley");
 var formattedRole = HTMLheaderRole.replace("%data%", "Application Developer");
 
 $("#main").prepend(formattedRole);
-$("#main").prepend(formattedName); n
+$("#main").prepend(formattedName); 
 
-var s = "audacity";
+/*var s = "audacity";
 
 var udacityizer = function(s){
 
@@ -56,16 +56,38 @@ var bio={
 	"contact info": "nieshaamos@gmail.com",
 	"picture URL": null,
 	"welcome message":null,
-	"skills":null
+	"skills":["HTML","CSS","Javascript"]
 
 };
 
-var work={};
+var work={
+	"jobs":[
+	{
+		"position":"Lead Web Desingner and Programmer",
+		"employer": "Motor City Dev Chix",
+		"location":"motorcitydevchix.com",
+		"dates worked":"2014-current",
+		"job description":"Manage new and existing projects for professional web design services"
+	},
+	{
+		"position":"Application Developer",
+		"employer": "GE Capital",
+		"location":"Van Buren Township",
+		"dates worked":"2012-current",
+		"job description":"Build and manage business rule engine and manage Underwriting Web Services for Touchless" +
+		"systems"
+	},
+	{
+		"position":"Web Design Intern",
+		"employer":"Crain Communications",
+		"location":"Detroit",
+		"dates worked":"2012",
+		"job description":"Implemented and installed plugins for company publications"
+	}
 
-	work.position="Application Developer";
-	work.employer="GE Capital";
-	work.yearsWorked=3;
-	work.city="Van Buren Township";
+
+	]
+}
 
 var education={
 	"schools":[
@@ -88,3 +110,58 @@ var education={
 var projects={
 	"portfolioURL":"motorcitydevchix.com"
 }
+
+if(bio.skills.length > 0){
+	$("#header").append(HTMLskillsStart);
+
+	var formattedSkill;
+	for(skill in bio.skills){
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+	$("#skills").append(formattedSkill);
+	}
+	
+}
+
+function displayWork(){
+	var formattedEmployer, formattedTitle, formattedWorkDates, formattedLocation, formattedJobDesc;
+	for(job in work.jobs){
+	$('#workExperience').append(HTMLworkStart);
+
+	formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+	formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].position);
+	formattedWorkDates = HTMLworkDates.replace("%data%",work.jobs[job]["dates worked"])
+	formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+	formattedJobDesc = HTMLworkDescription.replace("%data%",work.jobs[job]["job description"]);
+
+	$(".work-entry:last").append(formattedEmployer +  formattedTitle  + formattedLocation  
++ formattedWorkDates +  formattedJobDesc);
+
+
+	}
+}
+
+displayWork();
+
+/*$(document).click(function(loc) {
+  logClicks(loc.pageX, loc.pageY);
+});*/
+
+
+
+function inName(){
+
+	var internationalName;
+
+	var name = $('#name').text();
+
+	var spaceIndex = name.indexOf(' ');
+
+	internationalName = name.slice(0,1).toUpperCase() + name.slice(1, spaceIndex) + " " + 
+	name.slice(spaceIndex + 1).toUpperCase();
+
+console.log(internationalName);
+	return internationalName;
+}
+
+$("#main").append(internationalizeButton);
+
